1002cd15fc9104a2c90db66c6f8d175b97f8c36d
Matt <matthewjamesbrake@icloud.com>
2017-10-03 19:27:29 +0100

    Movement basics complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float maxSpeed;
+	public float speedReduction;
+	public float tilt;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal;
+
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			moveHorizontal = speedReduction;
+		}
+		else if(Input.GetKey(KeyCode.W))
+		{
+			moveHorizontal = speedReduction;
+		}
+		else moveHorizontal = Input.GetAxis("Vertical");
+		
+		float moveVertical = -Input.GetAxis("Horizontal");
+
+		Vector3 movement = new Vector3
+		(
+			moveHorizontal, 
+			0.0f, 
+			moveVertical 
+		);
+
+		rb.velocity = movement * maxSpeed;
+		rb.rotation = Quaternion.Euler(rb.velocity.z * tilt, 0.0f, 0.0f);
+	}
+}
